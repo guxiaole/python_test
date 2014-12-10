900fb6a6d8259f55c05f4d5bb8ccc8c26e51b0ac
xiaole <455027298@qq.com>
2014-12-10 14:11:34 +0800

    add some files

--- backup_ver4.py
+++ backup_ver4.py
@@ -5,11 +5,12 @@ import os
 import time
 
 # 1. The files and directories to be backed up are specified in a list.
-source=['/home/swaroop/byte','/home/swaroop/bin']
-# If you are using Windows, use source=[r'C:\Documents',r'D:\Work'] or something like that
+source=[r'E:\STUDY\python_test']
+# If you are using Windows, use source=[r'C:\Documents',r'D:\Work'] orthat
 
 # 2. The backup must be stored in a main backup directory
-target_dir='/mnt/e/backup/' #Remember to change this to what you will be using
+target_dir='E:\STUDY'
+#Remember to change this to what you will be using
 
 # 3. The files are backed up into a zip file
 # 4. The current day is the name of the subdirectory in the main directory
@@ -19,7 +20,8 @@ now=time.strftime('%H%M%S')
 
 # Take a comment from the user to create the name of the zip file
 comment=raw_input('Enter a comment --> ')
-if len(comment)==0: # check if a comment was entered
+if len(comment)==0:
+# check if a comment was entered
 	target=today+os.sep+now+'.zip'
 else:
 	target=today+os.sep+now+'_'+\
