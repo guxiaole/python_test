775b5cba99fa108c4f0037f462b02fe7eea4191b
xiaole <455027298@qq.com>
2014-12-11 20:14:57 +0800

    update the readTextfile.py

--- readTextFile.py
+++ readTextFile.py
@@ -5,17 +5,27 @@
 import os
 ls=os.linesep
 #path='E:\STUDY\\'
-path =raw_input("input the path of file like this\n '>>C:\workpath'\n>>")
+path =raw_input("input the path of file and name like this\n '>>C:\workpath\ test.text'\n>>")
 
-fname=path+raw_input('input the filename which you want: ')
+#fname=path+raw_input('input the filename which you want: ')
 
-#file to read
-if os.path.exists(fname):
+#file to readlines
+
+try:
 	fobj=open(fname,'r')
+except IOError,e:
+	print "file open failed:",e
+else:
 	for eachline in fobj:
 		print eachline,
 	fobj.close()
-	#file=fobj.readlines(209715200)
-	#print '%s%s' %(file,ls)
-else:
-	print "ERROR: '%s' not exists" % fname
+
+#if os.path.exists(fname):
+	#fobj=open(fname,'r')
+	#for eachline in fobj:
+		#print eachline,
+	#fobj.close()
+	##file=fobj.readlines(209715200)
+	##print '%s%s' %(file,ls)
+#else:
+	#print "ERROR: '%s' not exists" % fname
